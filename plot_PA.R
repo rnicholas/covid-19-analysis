20d254d0429e6f79db6c6cd07f7790eadbbaf65b
Robert Nicholas <ren10@psu.edu>
2020-05-04 08:09:40 -0400

    thought I already added plot_PA.R

--- plot_PA.R
+++ plot_PA.R
@@ -1,6 +1,6 @@
 # Create plots of various COVID-19 stats for all PA counties.
 #
-# Last modified 22 April 2020 by Rob Nicholas <rob.nicholas@gmail.com>.
+# Last modified 3 May 2020 by Rob Nicholas <rob.nicholas@gmail.com>.
 
 # Clear the environment and close all existing plots.
 rm(list=ls())
@@ -11,6 +11,7 @@ graphics.off()
 ts_path <- '../COVID-19/csse_covid_19_data/csse_covid_19_time_series/'
 us_cases <- read.csv(paste(ts_path,'time_series_covid19_confirmed_US.csv',sep=""))
 us_deaths <- read.csv(paste(ts_path,'time_series_covid19_deaths_US.csv',sep=""))
+
 
 
 # Identify and select those rows with PA data (mostly counties).
@@ -32,10 +33,11 @@ dtd_aggregate_cases <- aggregate_cases[2:l] - aggregate_cases[1:l-1]
 dtd_aggregate_deaths <- aggregate_deaths[2:l] - aggregate_deaths[1:l-1]
 
 # Plot state cases and deaths.
-plot(dtd_aggregate_cases[-(1:40)], xlab="days since 3/1/2020", ylab="new cases", main="COVID-19 in Pennsylvania")
+plot(dtd_aggregate_cases[-(1:40)], xlab="days since 3/1/2020", ylab="new cases", main="COVID-19 in Pennsylvania", pch=16, col="blue", type="o")
+grid()
 x11()
-plot(dtd_aggregate_deaths[-(1:40)], xlab="days since 3/1/2020", ylab="deaths", main="COVID-19 in Pennsylvania")
-
+plot(dtd_aggregate_deaths[-(1:40)], xlab="days since 3/1/2020", ylab="deaths", main="COVID-19 in Pennsylvania", pch=16, col="dark red", type="o")
+grid()
 
 # Plot Centre County cases and deaths.
 locality_row <- which(subset_ids == "Centre")
@@ -45,8 +47,9 @@ locality_deaths <- as.vector(t(subset_deaths[locality_row,]))
 dtd_locality_deaths <- locality_deaths[2:l] - locality_deaths[1:l-1]
 
 x11()
-plot(dtd_locality_cases[-(1:40)], xlab="days since 3/1/2020", ylab="new cases", main="COVID-19 in Centre County")
+plot(dtd_locality_cases[-(1:40)], xlab="days since 3/1/2020", ylab="new cases", main="COVID-19 in Centre County", pch=16, col="blue", type="o")
+grid()
 x11()
-plot(dtd_locality_deaths[-(1:40)], xlab="days since 3/1/2020", ylab="deaths", main="COVID-19 in Centre County")
-
+plot(dtd_locality_deaths[-(1:40)], xlab="days since 3/1/2020", ylab="deaths", main="COVID-19 in Centre County", pch=16, col="dark red", type="o")
+grid()
 
